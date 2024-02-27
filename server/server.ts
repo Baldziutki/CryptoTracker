49335d9435af8fc9fd4070b757fac6d723640691
Jakub Lysakowski <107717515+Baldziutki@users.noreply.github.com>
2024-02-27 21:26:39 +0100

    feat: jwt + cookies

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,10 @@
 import fastify from 'fastify';
+import cors from '@fastify/cors'
+import fastifyAuth from '@fastify/auth';
+import fastifyCookie from '@fastify/cookie';
+import fastifyJwt from '@fastify/jwt';
 import dotenv from 'dotenv';
+import { FastifyRequest, FastifyReply } from 'fastify';
 
 export const build = () => {
 
@@ -15,11 +20,45 @@ export const build = () => {
         logger: true
     });
 
+    server.register(cors, {
+        origin: async (origin) => {
+            if (origin === undefined) {
+                return true;
+            }
+            console.log(origin);
+            const hostname = new URL(origin).hostname
+            if (hostname !== 'localhost' && hostname !== '127.0.0.1') {
+                throw new Error('Origin not allowed');
+            };
+            return true;
+        },
+        credentials: true
+    });
+
+    server.register(fastifyAuth);
+    server.register(fastifyCookie);
+
+    server.register(fastifyJwt, {
+        secret: 'foobar',
+        cookie: {
+            cookieName: 'token',
+            signed: false,
+        },
+    });
+
+    server.decorate('verifyJWT', async function (request: FastifyRequest, reply: FastifyReply) {
+        try {
+            await request.jwtVerify();
+        } catch (error) {
+            reply.send(error);
+        }
+    });
+
     server.register(require('@fastify/mongodb'), {
         forceClose: true,
         url: dbUrl
-      })
-    
+    });
+
 
     return server;
 };
