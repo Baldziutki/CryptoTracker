33ffa9eee99cb5cd9faf24518f90b1d02565cab3
Jakub Lysakowski <107717515+Baldziutki@users.noreply.github.com>
2024-03-26 19:17:46 +0100

    feat: percentage

---
+++ client/src/components/renderPercentage/RenderPercentage.tsx
@@ -0,0 +1,21 @@
+
+import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
+
+
+export const RenderPercentage = ({ number, _class }: { number: number, _class: string }) => {
+    if (number < 0) {
+        return (
+            <div className={_class}>
+                <ChevronDownIcon color='red' />
+                <span className='text-red-500'>{number?.toFixed(2)}%</span>
+            </div>
+        )
+    } else {
+        return (
+            <div className={_class}>
+                <ChevronUpIcon color='green' />
+                <span className='text-green-500'>{number?.toFixed(2)}%</span>
+            </div>
+        )
+    }
+};
