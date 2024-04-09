3acf4e8970d89623bee508fccc42620897115a00
Jakub Lysakowski <107717515+Baldziutki@users.noreply.github.com>
2024-04-09 17:35:49 +0200

    feat: new icons

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import SearchForm from "./searchForm/SearchForm"
 import SearchButton from "./searchButton/SearchButton";
-
+import { Wallet, BitcoinFill, Coin, ArrowRightLeft } from "akar-icons";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -11,13 +11,32 @@ export default function Navbar() {
     return (
         <div className="flex flex-row pt-2">
             <div className="flex flex-row items-center justify-center gap-6">
-                <a href="/">Cryptocurrencies</a>
-                <a href="/exchanges">Exchanges</a>
-                <a href="/wallet">Wallet</a>
+                <a href="/">
+                    <div className="flex flex-row items-center gap-1">
+                        <BitcoinFill strokeWidth={2} size={24} />
+                        <span>Cryptocurrencies</span>
+                    </div>
+                </a>
+                <a href="/exchanges">
+                    <div className="flex flex-row items-center gap-1">
+                        <div className="flex flex-row">
+                        <Coin strokeWidth={2} size={24} />
+                        <ArrowRightLeft strokeWidth={2} size={24} />
+                        <BitcoinFill strokeWidth={2} size={24} />
+                        </div>
+                        Exchanges
+                    </div>
+                </a>
+                <a href="/wallet">
+                    <div className="flex flex-row items-center gap-1">
+                        <Wallet strokeWidth={2} size={24} />
+                        <span>Wallet</span>
+                    </div>
+                </a>
             </div>
             <div className="grow" />
-            <SearchButton setIsOpen={setIsOpen}/>
-            {isOpen ? <SearchForm isOpen={isOpen} setIsOpen={setIsOpen}/> : null}
+            <SearchButton setIsOpen={setIsOpen} />
+            {isOpen ? <SearchForm isOpen={isOpen} setIsOpen={setIsOpen} /> : null}
         </div>
     )
 }
